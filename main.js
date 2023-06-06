fb93a5a51c3a246a0be20ab9fd415d821c70152c
ReubenMay <95076512+ReubenMay@users.noreply.github.com>
2023-06-06 04:35:46 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
+difference=0;
+leftwristx=0;
+rightwristx=0;
 function setup(){
     video=createCapture(VIDEO)
     video.size(550,500)
     
     canvas=createCanvas(500,500);
     canvas.position(600,100);
+    
     poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on("pose",gotPoses)
+    poseNet.on("pose",gotPoses);
     
 }
 function modelLoaded(){
     console.log("poseNet Is initalized")
 }
 function draw(){
-    background("#f7853e")
+    background("#e0812d")
+    document.getElementById("square_side").innerHTML="font size will be = "+difference+"px"
+    textSize(difference);
+    fill("#11249c")
+    text("Reuben",180,280);
 }
 function gotPoses(results){
     if(results.length>0){
         console.log(results)
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+        console.log("leftwristx=     " +leftwristx+"rightwristx=   "+rightwristx+
+        "difference=     "+difference);
     }
 }
